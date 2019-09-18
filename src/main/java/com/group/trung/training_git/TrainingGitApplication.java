4efd1c2a735f80c8d0cc4069d09dee1533d3a14b
pumaover <pumaover@gmail.com>
2019-09-18 13:15:52 +0700

    first commit

---
+++ src/main/java/com/group/trung/training_git/TrainingGitApplication.java
@@ -0,0 +1,13 @@
+package com.group.trung.training_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group/trung/training_git/TrainingGitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.group.trung.training_git;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrainingGitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
